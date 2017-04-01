349388e1d37f8be9ef4ff169215812d9e7693f35
shen yi fei <m809745357@gmail.com>
2017-04-01 08:35:51 +0000

    Add User Following

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
-  @if(Auth::check())
+  @if (Auth::check())
     <div class="row">
       <div class="col-md-8">
         <section class="status_form">
@@ -14,13 +14,16 @@
         <section class="user_info">
           @include('shared.user_info', ['user' => Auth::user()])
         </section>
+        <section class="stats">
+          @include('shared.stats', ['user' => Auth::user()])
+        </section>
       </aside>
     </div>
   @else
     <div class="jumbotron">
       <h1>Hello Laravel</h1>
       <p class="lead">
-        你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的示例项目主页。
+        你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的项目主页。
       </p>
       <p>
         一切，将从这里开始。
